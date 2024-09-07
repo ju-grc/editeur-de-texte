3e8f2111b76137d618d2b5f541cce756cb2e69cf
Julien Garcia <julien.garcia05@etu.umontpellier.fr>
2024-09-07 18:48:23 +0200

    refactor : commande effacer()

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -40,9 +40,7 @@ public class Document {
     }
 
     public void effacer(int debut, int fin) {
-        String partieGauche = texte.substring(0, debut);
-        String partieDroite = texte.substring(fin +1);
-        texte = partieGauche + partieDroite;
+        remplacer(debut,fin," ");
     }
 
     public void clear(){

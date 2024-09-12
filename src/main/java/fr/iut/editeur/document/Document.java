d0c73c481b6fd409de085ce88b8745b64d946b61
Julien Garcia <julien.garcia05@etu.umontpellier.fr>
2024-09-12 09:01:31 +0200

    docs : javadoc

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -25,12 +25,23 @@ public class Document {
         return this.texte;
     }
 
+    /**
+     * Remplace le texte qui se trouve entre les index debut et fin par un nouveau message
+     * @param debut index du debut du remplacement
+     * @param fin index de fin du remplacement
+     * @param remplacement message
+     */
     public void remplacer(int debut, int fin, String remplacement) {
         String partieGauche = texte.substring(0, debut);
         String partieDroite = texte.substring(fin + 1);
         texte = partieGauche + remplacement + partieDroite;
     }
 
+    /**
+     * Remplace le texte qui se trouve entre les index debut et fin par des majuscules
+     * @param debut index du debut du remplacement
+     * @param fin index de fin du remplacement
+     */
     public void majuscules(int debut, int fin) {
         String partieGauche = texte.substring(0, debut);
         String partieDroite = texte.substring(fin +1);
@@ -39,10 +50,18 @@ public class Document {
 
     }
 
+    /**
+     * Effacer la partie du texte qui se trouve entre les index debut et fin
+     * @param debut index du debut de l'effacement
+     * @param fin index de la fin de l'effacement
+     */
     public void effacer(int debut, int fin) {
         remplacer(debut,fin,"");
     }
 
+    /**
+     * Effacer le texte en integralité
+     */
     public void clear(){
         texte = "";
     }
